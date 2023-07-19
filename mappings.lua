47e5a473ed86ecb9e36b2cf98e375c0be968efaf
gabriel-tapes <gabrielsouza.iacs@gmail.com>
2023-07-19 11:11:23 -0300

    show home if all buffers are closed

---
+++ mappings.lua
@@ -0,0 +1,14 @@
+return {
+  n = {
+    ["<leader>c"] = {
+      function()
+        local bufs = vim.fn.getbufinfo { buflisted = true }
+        require("astronvim.utils.buffer").close()
+        if require("astronvim.utils").is_available "alpha-nvim" and not bufs[2] then
+          require("alpha").start(true)
+        end
+      end,
+      desc = "Close buffer"
+    }
+  }
+}
