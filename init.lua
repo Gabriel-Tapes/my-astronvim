927b2f849cc35e0baf1562e0ebb9687318923741
gabriel-tapes <gabrielsouza.iacs@gmail.com>
2023-07-19 11:08:58 -0300

    configure cursor, numbers and clipboard integration

--- init.lua
+++ init.lua
@@ -1,3 +1,11 @@
 return {
   colorscheme = 'github_dark_high_contrast',
+  options = {
+    opt = {
+      number = true,
+      relativenumber = false,
+      guicursor = { 'n-v-c:hor20,i-ci-ve:ver25,r-cr-o:hor20' },
+      clipboard = "unnamedplus"
+    }
+  },
 }
