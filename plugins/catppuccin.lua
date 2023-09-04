e5d0db8280852e0d403a7f475740d671a2577d7a
gabriel-tapes <gabrielsouza.iacs@gmail.com>
2023-09-03 21:43:46 -0300

    add catppuccin theme

---
+++ plugins/catppuccin.lua
@@ -0,0 +1,29 @@
+return {
+  'catppuccin/nvim',
+  name = 'catppuccin',
+  priority = 1000,
+  opts = {
+    term_colors = true,
+    transparent_background = false,
+    styles = {
+      comments = {},
+      conditionals = {},
+      loops = {},
+      functions = {},
+      keywords = {},
+      strings = {},
+      variables = {},
+      numbers = {},
+      booleans = {},
+      properties = {},
+      types = {}
+    },
+    color_overrides = {
+      mocha = {
+        base = '#000000',
+        mantle = '#000000',
+        crust = '#000000'
+      }
+    }
+  }
+}
