4e2275e61f314042cfa764e43c72fa20f1996f00
gabriel-tapes <gabrielsouza.iacs@gmail.com>
2023-09-03 21:42:58 -0300

    move neotest setup

--- plugins/neotest.lua
+++ plugins/neotest.lua
@@ -1,25 +1,6 @@
 return {
   "nvim-neotest/neotest",
-  config = function()
-    -- get neotest namespace (api call creates or returns namespace)
-    local neotest_ns = vim.api.nvim_create_namespace("neotest")
-    vim.diagnostic.config({
-      virtual_text = {
-        format = function(diagnostic)
-          local message = diagnostic.message:gsub("\n", " "):gsub("\t", " "):gsub("%s+", " "):gsub("^%s+", "")
-          return message
-        end,
-      },
-    }, neotest_ns)
-    require("neotest").setup {
-      adapters = {
-        require("neotest-jest"),
-        require("neotest-vitest"),
-        require("neotest-python"),
-      },
-      log_level = vim.log.levels.DEBUG,
-    }
-  end,
+  config = require "user.plugins.configs.neotest",
   ft = { "jest", "vitest", "python" },
   dependencies = {
     "nvim-treesitter/nvim-treesitter",
